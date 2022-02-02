239dacdb1f56489a9e2d4659915a1e475c765892
Giulio Zausa <mail.zausa.giulio@gmail.com>
2022-02-03 00:38:11 +0100

    Fixed fat

--- imgtool/create.sh
+++ imgtool/create.sh
@@ -3,8 +3,28 @@
 cd /mnt/imgtool
 
 rm os.img
-dd if=/dev/zero of=os.img bs=32K count=1156
+dd if=/dev/zero of=os.img bs=32K count=1200
 dd conv=notrunc if=clean_fat.img of=os.img
+
+# C=count/16.2
+# LBA=count*63.590625=count*32*1024/512
+
+# Cylinder -> 1 byte at 0x1c5
+printf '\x4a' | dd of=os.img bs=1 seek=453 count=1 conv=notrunc
+# LBA -> 4 bytes at 0x1cA, 4 bytes at 0x7e20
+printf '\x00\x2c\x01\x00' | dd of=os.img bs=1 seek=458 count=4 conv=notrunc
+printf '\x00\x2c\x01\x00' | dd of=os.img bs=1 seek=32288 count=4 conv=notrunc
+# Sectors per FAT -> 1 byte at 0x1cA (0x64 or 0x48)
+printf '\x64' | dd of=os.img bs=1 seek=32278 count=4 conv=notrunc
+
+# 1156 = C 71, LBA 73521
+# 1600 = C 99, LBA 101745
+
+# 1600/99=1156/71
+# C=~16.2x
+
+# 101745/1600=73521/1156
+# LBA=0.01572
 
 # dd conv=notrunc if=mbr.img of=os.img
 # (
@@ -18,7 +38,7 @@ dd conv=notrunc if=clean_fat.img of=os.img
 # echo 1 # Partition number
 # echo w # Write changes
 # ) | fdisk -H 16 -S 63 os.img
-# printf '\x0e' | dd of=os.img bs=1 seek=450 count=1 conv=notrunc 
+# printf '\x0e' | dd of=os.img bs=1 seek=450 count=1 conv=notrunc
 
 # mkfs.fat -F 16 -g 16/63 --offset 63 os.img
 # mformat -h 16 -s 63 -H 0 -c 4 -i "os.img@@32256" ::
