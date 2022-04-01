4b771429765afa3e7f4fb1185dd0156e6ec8638f
Giulio Zausa <mail.zausa.giulio@gmail.com>
2022-04-01 22:12:54 +0200

    Fixed path

--- tools/build-binaries.sh
+++ tools/build-binaries.sh
@@ -5,7 +5,7 @@ rm -f win-runtime/filesystem/WINDOWS/Start\ Menu/Programs/StartUp/bridge.exe
 mkdir -p win-runtime/filesystem/WINDOWS/Start\ Menu/Programs/StartUp/
 mv -f win-runtime/bridge/bridge.exe win-runtime/filesystem/WINDOWS/Start\ Menu/Programs/StartUp/bridge.exe
 
-if test -f "../../win-runtime/filesystem/WINDOWS/SYSTEM.DAT"; then
+if test -f "win-runtime/filesystem/WINDOWS/SYSTEM.DAT"; then
   echo "Registry already exists, skipping"
 else
   docker run --rm -v $(pwd):/mnt w95-imgtool sh reg.sh
