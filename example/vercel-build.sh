17ba69107739ca2c1b97e0f2e27fe3af0cce7e35
Giulio Zausa <mail.zausa.giulio@gmail.com>
2022-04-01 17:46:49 +0200

    Typo

--- example/vercel-build.sh
+++ example/vercel-build.sh
@@ -1,13 +1,11 @@
-#!/bin.sh
+#!/bin/sh
 
 amazon-linux-extras install -y docker
 
 cd ..
 tools/build-tools.sh
 tools/build-binaries.sh
-yarn install
 yarn build
 
 cd example
-yarn install
 yarn build
