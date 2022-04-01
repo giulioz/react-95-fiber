f67e24041b44c2967ca6bafa558314d9ad688edc
Giulio Zausa <mail.zausa.giulio@gmail.com>
2022-04-01 16:52:57 +0200

    Fixed workflow again

--- tools/build-binaries.sh
+++ tools/build-binaries.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run --rm -it -v $(pwd):/prj giulioz/vc6-docker wine cmd /c z:/prj/win-runtime/bridge/build.bat
+docker run --rm -v $(pwd):/prj giulioz/vc6-docker wine cmd /c z:/prj/win-runtime/bridge/build.bat
 rm -f win-runtime/filesystem/WINDOWS/Start\ Menu/Programs/StartUp/bridge.exe
 mkdir -p win-runtime/filesystem/WINDOWS/Start\ Menu/Programs/StartUp/
 mv -f win-runtime/bridge/bridge.exe win-runtime/filesystem/WINDOWS/Start\ Menu/Programs/StartUp/bridge.exe
@@ -8,9 +8,9 @@ mv -f win-runtime/bridge/bridge.exe win-runtime/filesystem/WINDOWS/Start\ Menu/P
 if test -f "../../win-runtime/filesystem/WINDOWS/SYSTEM.DAT"; then
   echo "Registry already exists, skipping"
 else
-  docker run -it --rm -v $(pwd):/mnt w95-imgtool sh reg.sh
+  docker run --rm -v $(pwd):/mnt w95-imgtool sh reg.sh
 fi
 
-docker run -it --rm -v $(pwd):/mnt w95-imgtool sh create.sh
+docker run --rm -v $(pwd):/mnt w95-imgtool sh create.sh
 mv -f tools/imgtool/os.img binaries/os.img
 mkdir -p binaries
