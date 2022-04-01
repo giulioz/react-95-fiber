68497aa4fd3bd2f3d7b98184c3f7449cd98606c1
Giulio Zausa <mail.zausa.giulio@gmail.com>
2022-04-01 23:28:02 +0200

    16 MB disk image

--- tools/imgtool/create.sh
+++ tools/imgtool/create.sh
@@ -3,17 +3,17 @@
 cd /mnt/tools/imgtool
 
 rm os.img
-dd if=/dev/zero of=os.img bs=32K count=1024
+dd if=/dev/zero of=os.img bs=32K count=512
 dd conv=notrunc if=fat.img of=os.img
 
 # C=count/16.2
 # LBA=count*63.590625=count*32*1024/512
 
 # Cylinder -> 1 byte at 0x1c5
-printf '\3e' | dd of=os.img bs=1 seek=453 count=1 conv=notrunc
+printf '\1f' | dd of=os.img bs=1 seek=453 count=1 conv=notrunc
 # LBA -> 4 bytes at 0x1cA, 4 bytes at 0x7e20
-printf '\x00\x00\x01\x00' | dd of=os.img bs=1 seek=458 count=4 conv=notrunc
-printf '\x00\x00\x01\x00' | dd of=os.img bs=1 seek=32288 count=4 conv=notrunc
+printf '\x00\x80\x00\x00' | dd of=os.img bs=1 seek=458 count=4 conv=notrunc
+printf '\x00\x80\x00\x00' | dd of=os.img bs=1 seek=32288 count=4 conv=notrunc
 # Sectors per FAT -> 1 byte at 0x1cA (0x64 or 0x48)
 printf '\x64' | dd of=os.img bs=1 seek=32278 count=4 conv=notrunc
 
